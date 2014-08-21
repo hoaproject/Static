37d3372e0b9c6a6b391ea9a5433a31eea9c0daf2
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-08-21 10:35:55 +0200

    Add Piwik.

--- Javascript/Keynote.js
+++ Javascript/Keynote.js
@@ -512,3 +512,16 @@ $$.forEach = function ( nodeList, fun ) {
 
     Array.prototype.forEach.call(nodeList, fun);
 }
+
+
+// Piwik.
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://analytics.hoa-project.net/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 4]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
